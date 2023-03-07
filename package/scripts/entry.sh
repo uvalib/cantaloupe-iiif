e7e7681b49070c8e7711e9207b6dec2f45e2fc88
Dave Goldstein <dpg3k@virginia.edu>
2023-03-07 10:18:55 -0500

    Correctly sized -Xmx option

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -3,7 +3,7 @@
 #
 
 export LD_LIBRARY_PATH=/usr/local/lib
-cd bin; java -Dcantaloupe.config=cantaloupe.properties -Xmx4g -jar cantaloupe.jar
+cd bin; java -Dcantaloupe.config=cantaloupe.properties -Xmx2g -jar cantaloupe.jar
 
 #
 # end of file
